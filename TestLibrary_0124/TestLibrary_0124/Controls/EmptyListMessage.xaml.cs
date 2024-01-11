715b68f46c08610cd7a02fbac8cdf8423391640a
Divyesh Bhatt <divyesh.bhatt23@gmail.com>
2024-01-11 13:57:55 +0530

    Added custom control

---
+++ TestLibrary_0124/TestLibrary_0124/Controls/EmptyListMessage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLibrary_0124.Controls;
+
+public partial class EmptyListMessage : ContentView
+{
+	public EmptyListMessage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+	}
+}
